d5a90452677a067ca0e4a802a8b8148561203ded
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-05 15:27:17 -0700

    add simepler method from breakout

--- typewriter.js
+++ typewriter.js
@@ -1,17 +1,23 @@
 // Implement the logic to have each letter delay its appearance by 50ms (or so) longer than the previous letter.
 const sentence = "hello there from lighthouse labs";
 
-let index = 0;
+// let index = 0;
 
-// printing each letter one at a time
-const typewriter = setInterval(() => {
-  if (index < sentence.length) {
-    process.stdout.write(sentence[index]);
-    index++;
-  }
+// // printing each letter one at a time
+// const typewriter = setInterval(() => {
+//   if (index < sentence.length) {
+//     process.stdout.write(sentence[index]);
+//     index++;
+//   }
   
-  if (index === sentence.length) {
-    process.stdout.write("\n"); // new line at the end
-    clearInterval(typewriter);
-  }
-}, 50);
+//   if (index === sentence.length) {
+//     process.stdout.write("\n"); // new line at the end
+//     clearInterval(typewriter);
+//   }
+// }, 50);
+
+let counter = 50;
+for (const letter of sentence) {
+  setTimeout(() => process.stdout.write(letter), counter);
+  counter += 50;
+}
